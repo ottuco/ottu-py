85444e5b79ad01a523d513cfb9bd140d6c5464d1
Jerin Peter George <jerinpetergeorge@gmail.com>
2023-11-06 20:04:10 +0530

    Remove `KeyCloakAuth` as it will be defined within other package

--- src/ottu/auth.py
+++ src/ottu/auth.py
@@ -1,4 +1,3 @@
-import httpx
 from httpx import Auth, BasicAuth as _BasicAuth, Request
 
 
@@ -12,38 +11,4 @@ class APIKeyAuth(Auth):
 
     def auth_flow(self, request: Request):
         request.headers["Authorization"] = f"Api-Key {self.api_key}"
-        yield request
-
-
-class KeyCloakAuth(Auth):
-    def __init__(
-        self,
-        username: str,
-        password: str,
-        keycloak_host: str,
-        realm: str,
-        client_id: str = "frontend",
-    ):
-        self.username = username
-        self.password = password
-        self.keycloak_host = keycloak_host
-        self.realm = realm
-        self.client_id = client_id
-        self._url = f"{self.keycloak_host}/auth/realms/{self.realm}/protocol/openid-connect/token"
-
-    def get_access_token(self):
-        response = httpx.post(
-            url=self._url,
-            data={
-                "grant_type": "password",
-                "client_id": self.client_id,
-                "username": self.username,
-                "password": self.password,
-            },
-        )
-        return response.json()["access_token"]
-
-    def auth_flow(self, request: Request):
-        access_token = self.get_access_token()
-        request.headers["Authorization"] = f"Bearer {access_token}"
         yield request
