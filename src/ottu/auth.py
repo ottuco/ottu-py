04112cc84b0bf89d75d8dcf2d6cccf9294cc2f99
Jerin Peter George <jerinpetergeorge@gmail.com>
2024-02-04 22:12:11 +0530

    Add custom Keycloak auth classes

--- src/ottu/auth.py
+++ src/ottu/auth.py
@@ -1,3 +1,6 @@
+import typing
+
+import httpx
 from httpx import Auth, BasicAuth as _BasicAuth, Request
 
 
@@ -28,3 +31,77 @@ class APIKeyAuth(Auth):
 
     def __bool__(self):
         return bool(self.api_key)
+
+
+class KeycloakAuthBase:
+    grant_type: str
+
+    def __init__(self, host: str, realm: str, *args, **kwargs):
+        self.host = host
+        self.realm = realm
+
+    @property
+    def token_url(self) -> str:
+        return (
+            f"https://{self.host}/auth/realms/"
+            f"{self.realm}/protocol/openid-connect/token"
+        )
+
+    def get_token_request_payload(self) -> dict:
+        return {"grant_type": self.grant_type}
+
+    def get_token(self) -> str:
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        data = self.get_token_request_payload()
+        response = httpx.post(url=self.token_url, data=data, headers=headers)
+        response.raise_for_status()
+        return response.json().get("access_token")
+
+    def auth_flow(self, request: Request):
+        access_token = self.get_token()
+        request.headers["Authorization"] = f"Bearer {access_token}"
+        request.headers["X-Service-ID"] = self.realm
+        yield request
+
+
+class KeycloakPasswordAuth(KeycloakAuthBase, Auth):
+    grant_type = "password"
+
+    def __init__(
+        self,
+        username: str,
+        password: str,
+        client_id: str,
+        *args,
+        client_secret: typing.Optional[str] = None,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.username = username
+        self.password = password
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    def get_token_request_payload(self) -> dict:
+        payload = super().get_token_request_payload()
+        payload["username"] = self.username
+        payload["password"] = self.password
+        payload["client_id"] = self.client_id
+        if self.client_secret:
+            payload["client_secret"] = self.client_secret
+        return payload
+
+
+class KeycloakClientAuth(KeycloakAuthBase, Auth):
+    grant_type = "client_credentials"
+
+    def __init__(self, client_id: str, client_secret: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    def get_token_request_payload(self) -> dict:
+        payload = super().get_token_request_payload()
+        payload["client_id"] = self.client_id
+        payload["client_secret"] = self.client_secret
+        return payload
